edf75fd87596ce5e2439cbda13d08c5a9576479d
webManiac <wankhade.ashish@yahoo.com>
2025-07-04 01:24:32 +0530

    overall main structure

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public static void runMain() {
+        Main.main(new String[]{"sa", "AS"});
+    }
+
+    public static void main(String[] args) {
+        Main.main(new String[]{"sa", "AS"});
+    }
+}
